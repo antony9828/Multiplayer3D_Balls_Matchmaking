4a2929c17319fe1b0165a9c253bb007724405384
Anton <anton.pushkin2@gmail.com>
2019-06-24 12:02:25 +0300

    Initial commit

---
+++ Assets/Scripts/Actions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actions : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        var playerNumber = other.GetComponent<BallMovement>().playerNumber;
+        if (playerNumber == 1)
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
+}
---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BallMovement : NetworkBehaviour
+{
+    [SerializeField] private float speed = 10f;
+    private Rigidbody rb;
+    [SyncVar] public int playerNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<Renderer>().material.color = Color.green;
+
+        base.OnStartLocalPlayer();
+
+        if (isServer)
+            playerNumber = 1;
+        else
+            playerNumber = 0;
+    }
+}
+
